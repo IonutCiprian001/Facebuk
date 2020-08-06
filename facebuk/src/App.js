5804996200489e681f566070556c965046dae641
IonutCiprian001 <cipriben10@yahoo.com>
2020-08-06 16:42:30 +0200

    change formating style

--- facebuk/src/App.js
+++ facebuk/src/App.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    header className = "App-header" >
-    <
-    img src = {
-      logo
-    }
-    className = "App-logo"
-    alt = "logo" / >
-    <
-    p >
-    Sa mi - l sugi cu lapte. <
-    /p> <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> <
-    /header> <
-    /div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Sa mi - l sugi cu lapte. </p>{" "}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
+    </div>
   );
 }
 
